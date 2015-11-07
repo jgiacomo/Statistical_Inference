ba02b17e3c2b2bc5f01c9ec554a192799647c0b4
Jason Giacomo <jgiacomo@gmail.com>
2015-11-07 14:57:46 -0800

    Initial commit.

---
+++ StatInference_Test.R
@@ -0,0 +1,17 @@
+set.seed(546)
+
+means <- function(n){
+    expMeans <- 0
+    for(i in 1:n){
+        expMeans[i] <- mean(rexp(40, 0.2))
+    }
+    return(expMeans)
+}
+
+df <- data.frame("ExpMeans" = means(1000))
+
+p1 <- ggplot(data=df, aes(x=ExpMeans)) +
+        geom_histogram(aes(y=..density..), color="black", fill="white") +
+        geom_density(fill="red", alpha=0.2)
+
+print(p1)
